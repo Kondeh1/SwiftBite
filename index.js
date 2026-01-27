cef236d24ac10f1ae588d8a9506cb774d2e3b044
Moriba Sahr Kondeh <moribasahrkondeh0@gmail.com>
2026-01-27 05:37:02 +0000

    This is the initial version for SwiftBite

--- index.js
+++ index.js
@@ -3,17 +3,47 @@ import axios from "axios";
 import pg from "pg";
 import bodyParser from "body-parser";
 import env from "dotenv";
+import passport from "passport";
+import session from "express-session";
 
 env.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
+
+app.use(passport.session());
+app.use(passport.initialize());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to SwiftBite</h1>");
+const db = new pg.Client({
+  user: "postgres",
+  host: process.env.HOST_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: "SwiftBite",
+});
+
+db.connect();
+
+app.get("/", async (req, res) => {
+  const result = await db.query("SELECT user_name, user_role FROM users");
+  res.render("index.ejs");
+});
+
+app.post("/login", async (req, res) => {
+  const {email, password} = req.body;
+
+  console.log(email, password);
 });
 
 app.listen(port, () => {
